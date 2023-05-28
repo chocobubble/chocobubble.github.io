1df70b543efcf5bfda130c3c625f01cefc36fd40
Moon <100405650+chocobubble@users.noreply.github.com>
2023-05-28 14:37:09 +0900

    [2023. 5. 28.] 수학식 표시

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -23,8 +23,26 @@ var store = [{
         "url": "/ds/ds-test/",
         "teaser": "/assets/images/teaser.jpg"
       },{
+        "title": "자료 구조 - 배열",
+        "excerpt":"‘c++로 쉽게 풀어쓴 자료구조 1판 - 천인국, 최영규’ 책을 참고하여 작성한 포스트입니다. 배열(array) 배열은 하나의 변수에 여러 개의 값을 순차적으로 저장할 수 있는 자료 구조이다. 가장 기본적인 특징은 &lt;인덱스, 요소&gt; 쌍의 집합이라는 것이다. 즉, 인덱스가 주어지면 해당하는 요소가 대응되는 자료구조이다. C++의 배열에서는 모든 요소가 동일한 자료형이며, 인덱스를 사용하여 요소를 직접...","categories": ["DS"],
+        "tags": ["DS, Array"],
+        "url": "/ds/ds-2/",
+        "teaser": "/assets/images/teaser.jpg"
+      },{
+        "title": "자료 구조 - 스택",
+        "excerpt":"‘c++로 쉽게 풀어쓴 자료구조 1판 - 천인국, 최영규’ 책을 참고하여 작성한 포스트입니다. 스택 스택이란 자료의 입출력이 후입선출(LIFO:Last-In First-Out)의 형태로 일어나는 자료구조를 말한다. 스택 상단(stack top): 스택에서 입출력이 이루어지는 부분 스택 하단(stack bottom): top의 반대 부분 요소(element): 스택에 저장되는 것, 항목 공백(empty)상태: 스택에 요소가 하나도 없는 상태 포화(full)상태: 스택에 더 이상...","categories": ["DS"],
+        "tags": ["DS, Stack"],
+        "url": "/ds/ds-3/",
+        "teaser": "/assets/images/teaser.jpg"
+      },{
+        "title": "자료 구조 - 큐",
+        "excerpt":"‘c++로 쉽게 풀어쓴 자료구조 1판 - 천인국, 최영규’ 책을 참고하여 작성한 포스트입니다. 큐(Queue) 큐는 자료의 입출력이 선입선출(FIFO: First-In First-Out)의 형태로 일어나는 자료구조를 말한다. 큐에서 삽입이 일어나는 곳을 후단(rear)이라고 하고 삭제가 일어나는 곳을 전단(front)라고 한다. 큐의 추상 자료형 스택과 유사하다 데이터: 선입선출의 접근 방법을 유지하는 요소들의 모음 연산 - enqueue(e): 주어진...","categories": ["DS"],
+        "tags": ["DS, Queue"],
+        "url": "/ds/ds-4/",
+        "teaser": "/assets/images/teaser.jpg"
+      },{
     "title": "Data Structure",
-    "excerpt":"DS Test 1 less than 1 minute read For Test int main() { cout&lt;&lt;\"Hello\"; } 1 This post is for testing. And and and and 111 222 111 222 자료 구조 - 시작 3 minute read C++로 쉽게 풀어쓴 자료구조 1판 - 천인국, 최영규 DS Test 2 less than 1...","url": "http://localhost:4000/categories/DS"
+    "excerpt":"자료 구조 - 큐 1 minute read C++로 쉽게 풀어쓴 자료구조 1판 - 천인국, 최영규 자료 구조 - 스택 2 minute read C++로 쉽게 풀어쓴 자료구조 1판 - 천인국, 최영규 자료 구조 - 배열 less than 1 minute read C++로 쉽게 풀어쓴 자료구조 1판 - 천인국, 최영규 DS Test 1...","url": "http://localhost:4000/categories/DS"
   },{
     "title": "Category",
     "excerpt":" ","url": "http://localhost:4000/categories/"
@@ -55,4 +73,7 @@ var store = [{
   },{
     "title": null,
     "excerpt":"{% if page.xsl %}{% endif %}Jekyll{{ site.time | date_to_xmlschema }}{{ page.url | absolute_url | xml_escape }}{% assign title = site.title | default: site.name %}{% if page.collection != \"posts\" %}{% assign collection = page.collection | capitalize %}{% assign title = title | append: \" | \" | append: collection %}{% endif...","url": "http://localhost:4000/feed.xml"
+  },{
+    "title": null,
+    "excerpt":"","url": "http://localhost:4000/page2/"
   }]
